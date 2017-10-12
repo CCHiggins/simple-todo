f4df1c48cc5ba49aad6ee250da83bd0c6cc3b15f
Cameron Higgins <cameron.higgins@me.com>
2017-10-12 16:02:49 -0400

    adds publication and private tasks.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,8 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check'
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
 
 Meteor.methods({
   'tasks.insert'(text) {
@@ -23,13 +36,36 @@ Meteor.methods({
   'tasks.remove'(taskId) {
     check(taskId, String);
 
+    //if the task is private and the user isn't the owner
+    //then they aren't authorized to delete it!
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()){
+      throw new Meteor.Error('not authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked){
     check(taskId, String);
     check(setChecked, Boolean);
 
-
+    //if the task is marked private then only the owner
+    //can check that mother fucker off.
+    const task = Tasks.findOne(taskId)
+    if (task.private && task.owner !== Meteor.userId()){
+      throw new Meteor.Error('not authorized');
+    }
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setPrivate){
+    check(taskId, String);
+    check(setPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not authorized');
+    }
+    Tasks.update(taskId, { $set: { private: setPrivate } });
   },
 });
--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,10 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-
 import { Template } from 'meteor/templating';
 
-import { Tasks } from '../api/tasks';
-
 import './task.html';
+
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
 
 Template.task.events({
   'click .toggle-checked'() {
@@ -13,6 +16,9 @@ Template.task.events({
   },
   'click .delete'() {
     //call method to remove task
-    Meteor.call('tasks.remove',this._id);
+    Meteor.call('tasks.remove', this._id);
+  },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });
